4ff5c6a6ffa3a12c7ba0dd033df96485763f2ebf
tahay <mail@tahayasinerkan.com>
2017-11-13 18:26:46 +0300

    servletNotation

---
+++ src/servletNotation/ServletWithNotation.java
@@ -0,0 +1,21 @@
+package servletNotation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/hello")
+@WebServlet(urlPatterns = {"/hello","/merhaba"})
+public class ServletWithNotation extends HttpServlet {
+	public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		PrintWriter writer = response.getWriter();
+		writer.write("Hello Servlet World");
+		
+	}
+
+}
